15d3a5d406b482f00e1c3e864c67d98074b7938e
Philippa-Jennings <pcjennings@hotmail.com>
2024-03-18 16:18:32 +0000

    Add code to enable tests to run in pipeline
    Includes updating the config to ensure tests run headless AND adding code for GitHub Actions

--- wdio.conf.js
+++ wdio.conf.js
@@ -49,9 +49,17 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
-        browserName: 'chrome'
     
+    //capabilities: [{
+    //    browserName: 'chrome'
+    
+    //}],
+
+    capabilities: [{
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu']
+        }
     }],
 
     //
